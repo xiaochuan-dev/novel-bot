0ebdc4e6094b249561fd11da0c673202f83581f9
freexiaochuan <w2239559319@myyahoo.com>
2025-02-21 16:39:28 +0800

    f

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,5 @@
 async function startAction(url: string, env: Env) {
-	const fUrl = "https://api.github.com/repos/xiaochuan-dev/novel-bot/dispatches";
+	const fUrl = 'https://api.github.com/repos/xiaochuan-dev/novel-bot/dispatches';
 
 	// @ts-ignore
 	const token = env.GITHUB_ACCESS_TOKEN;
@@ -15,10 +15,10 @@ async function startAction(url: string, env: Env) {
 		method: 'POST',
 		body: JSON.stringify(data),
 		headers: {
-			'Authorization': `Bearer ${token}`,
-			'Accept': 'application/vnd.github.v3+json',
-			'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36'
-		}
+			Authorization: `Bearer ${token}`,
+			Accept: 'application/vnd.github.v3+json',
+			'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36',
+		},
 	});
 
 	const res = await r.text();
@@ -28,13 +28,28 @@ async function startAction(url: string, env: Env) {
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
 		const url = new URL(request.url);
+		// @ts-ignore
+		const telegram_token = env.TELEGRAM_TOKEN;
 
 		switch (url.pathname) {
-			case '/start': {
-				const urlParam = url.searchParams.get('url')!;
 
-				const res = await startAction(urlParam, env);
-				console.log(res);
+			case `/webhook/${telegram_token}`: {
+				const requestBody = await request.json();
+				console.log(requestBody);
+
+				break;
+			}
+
+			case '/start': {
+				if (request.method === 'GET') {
+					const urlParam = url.searchParams.get('url')!;
+					const res = await startAction(urlParam, env);
+					console.log(res);
+				}
+				if (request.method === 'POST') {
+					const requestBody = await request.json();
+					console.log(requestBody);
+				}
 
 				break;
 			}
