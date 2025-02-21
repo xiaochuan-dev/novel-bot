fbc13c3dbfcab7522eddae565219a48b2e16ce9b
freexiaochuan <w2239559319@myyahoo.com>
2025-02-21 19:04:54 +0800

    donw

--- action/index.js
+++ action/index.js
@@ -80,7 +80,7 @@ async function start() {
     return limit(async() => {
       const res = await getContent(name, url);
 
-      await new Promise(resolve => setTimeout(resolve, 2000));
+      await new Promise(resolve => setTimeout(resolve, 500));
 
       return res;
     });
