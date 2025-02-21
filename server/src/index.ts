b66d3b251f7c44bf9e3c558f3efa7f7ed4c0d7b6
freexiaochuan <w2239559319@myyahoo.com>
2025-02-21 15:14:32 +0800

    u

--- server/src/index.ts
+++ server/src/index.ts
@@ -17,6 +17,7 @@ async function startAction(url: string, env: Env) {
 		headers: {
 			'Authorization': `Bearer ${token}`,
 			'Accept': 'application/vnd.github.v3+json',
+			'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36'
 		}
 	});
 
