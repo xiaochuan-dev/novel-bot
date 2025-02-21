78ff3229ec3006ab023b465e25e8a0282ec68887
freexiaochuan <w2239559319@myyahoo.com>
2025-02-21 14:52:15 +0800

    u

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,44 @@
+async function startAction(url: string, env: Env) {
+	const fUrl = 'https://api.github.com/repos/OWNER/REPO/dispatches';
+
+	// @ts-ignore
+	const token = env.GITHUB_ACCESS_TOKEN;
+
+	const data = {
+		event_type: 'trigger_event',
+		client_payload: {
+			url,
+		},
+	};
+
+	const r = await fetch(fUrl, {
+		method: 'POST',
+		body: JSON.stringify(data),
+		headers: {
+			'Authorization': `token ${token}`,
+			'Accept': 'application/vnd.github.v3+json',
+		}
+	});
+
+	const res = await r.json();
+	return res;
+}
+
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+		const url = new URL(request.url);
+
+		switch (url.pathname) {
+			case '/start': {
+				const urlParam = url.searchParams.get('url');
+
+				break;
+			}
+
+			default: {
+				return new Response('Hello World');
+			}
+		}
+		return new Response('Hello World');
 	},
 } satisfies ExportedHandler<Env>;
